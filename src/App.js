a8a434059cfd326292d9cd64f44db21421dd9e12
Venal <s22310263@student.unklab.ac.id>
2024-11-17 00:07:30 +0800

    adding script andd app.js

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import initScripts from "./scripts";
+import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./components/About/About.js";
+import Contact from "./components/Contact/Contact.js";
+import Footer from "./components/Footer/Footer.js";
+import Headnav from "./components/Headnav/Headnav.js";
+import MasterHead from "./components/MasterHead/MasterHead.js";
+import Modal from "./components/Modal/Modal.js";
+import Portofolio from "./components/Portofolio/Portofolio.js";
+
+const App = () => {
+  useEffect(() => {
+    initScripts();
+  }, []);
+  return (
+    <div className="body-wrap">
+      <Headnav />
+      <MasterHead />
+      <Portofolio />
+      <About />
+      <Contact />
+      <Footer />
+      <Modal />
+    </div>
+  );
+};
+
+export default App;
